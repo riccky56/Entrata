2cfd06785b5c2a7ce38c4a81c19cd30984a87b71
ragupta1 <ragupta1@UI-2PN9KD4.us.deloitte.com>
2026-03-24 19:19:51 +0530

    new

--- src/main/java/Base/Browser.java
+++ src/main/java/Base/Browser.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public class Browser {
 
 	public static WebDriver driver;
